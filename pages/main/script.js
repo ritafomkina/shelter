2f2791d1340b5febf5799da5709ed802d6740099
ritafomkina <ritakisil@MacBook-Air-Margarita.local>
2022-11-26 15:38:49 +0200

    feat:burger menu

--- pages/main/script.js
+++ pages/main/script.js
@@ -9,9 +9,8 @@ let body = document.body;
 let popupWrapper = '<div class="wrapper popup-wrapper"><div class="popupHeaderWrapper"></div></div>';
 let popupBurger = document.querySelector("#header-burger").cloneNode(1);
 
-function openBurger(e) {
-    e.preventDefault();
-    // startFogging.classList.toggle("fogging");
+function openBurger(event) {
+    event.preventDefault();
     popup.classList.toggle("open");
     body.classList.toggle("noscroll");
     if(!popup.hasChildNodes()) {
@@ -27,6 +26,13 @@ function renderPopup() {
     popup.firstElementChild.firstElementChild.appendChild(headerLogo);
     popup.firstElementChild.firstElementChild.appendChild(popupBurger);
     popup.firstElementChild.appendChild(menu);
+    // console.log(document.querySelector(".start-link"));
+    document.querySelector(".start-link").classList.add("deactivated");
+    document.querySelector(".header-logo").classList.add("deactivated");
+    document.querySelector(".section-start").classList.add("shadow");
+    // console.log(document.querySelector(".header-logo").closest(".popup"));
+    // document.querySelector(".header-logo").setAttribute('disabled', '');
+    // document.querySelector(".start-link").setAttribute('disabled', '');
 };
 
 function removeMenu() {
